9ccf35d3834d1fea3c6cba0d8a375c0a216f259e
smckay4 <69876115+smckay4@users.noreply.github.com>
2020-08-19 10:58:56 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/*
+ * Author: Sawyer McKay
+ * 
+ * Date: 2020/08/18
+ *
+ * A simple hello world program written in C
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Sawyer McKay!\n");
+
+  return 0;
+}
